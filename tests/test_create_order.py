99890e60f5df425b13fbb9b24563438470dd535a
Elena Belozerova <107627792+Pillow3003@users.noreply.github.com>
2026-01-28 16:36:16 +0300

    Update test_create_order.py

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -3,17 +3,31 @@ import allure
 import pytest
 import requests
 from data import Orders
-from handle import Handle
+from tests.handle import Handle
 from urls import Urls
 
 
-class TestCreateOrder:
+@pytest.fixture
+def remove_order():
+    yield
+    # удалить через API невозможно, есть только отмена заказа, а это не имеет смысла, т.к. сущность
+    # продолжит существовать. Тут должно быть удаление напрямую чыерез БД.
 
-    @pytest.mark.parametrize('order_data', [{"color": ["BLACK"]}, {"color": ["GREY"]}, {"color": [""]}, {"color": ["BLACK", "GREY"]}])
+
+class TestCreateOrder:
+    @pytest.mark.parametrize(
+        'order_data',
+        [
+            {"color": ["BLACK"]}, {"color": ["GREY"]},
+            {"color": [""]}, {"color": ["BLACK", "GREY"]}
+         ]
+    )
     @allure.title('Создание заказа')
     def test_create_order(self, order_data):
         Orders.data_order.update(order_data)
         order_data = json.dumps(Orders.data_order)
         headers = {'Content-Type': 'application/json'}
         response = requests.post(f'{Urls.URL}{Handle.CREATE_ORDER}', data=order_data, headers=headers)
-        assert response.status_code == 201 and 'track' in response.text
+        assert response.status_code == 201
+        assert response.json().get("track") is not None
+        assert response.json().get("track") > 0
