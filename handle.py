00509e308f5c43dfbfdf1af53c5b75da34e17855
Elena Belozerova <107627792+Pillow3003@users.noreply.github.com>
2026-01-28 16:35:18 +0300

    Update handle.py

--- handle.py
+++ handle.py
@@ -2,6 +2,10 @@ class Handle:
 
     CREATE_COURIER = '/api/v1/courier'
 
+    DELETE_COURIER = "/api/v1/courier/{0}"
+
     LOGIN_COURIER = '/api/v1/courier/login'
 
     CREATE_ORDER = '/api/v1/orders'
+
+    DELETE_ORDER = ""
