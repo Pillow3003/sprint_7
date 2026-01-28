20d93d82f9ced45aa4eb97ae91f3240687cf2b5a
Elena Belozerova <107627792+Pillow3003@users.noreply.github.com>
2026-01-28 16:37:02 +0300

    Update test_login_courier.py

--- tests/test_login_courier.py
+++ tests/test_login_courier.py
@@ -1,31 +1,32 @@
 import allure
 import requests
 import pytest
-from handle import Handle
+from tests.handle import Handle
 from urls import Urls
-from data import Users
+from data import Users, Courier
 
 
 class TestLoginCourier:
+    @staticmethod
+    def _login_courier(*, request_body: dict) -> requests.Response:
+        return requests.post(f'{Urls.URL}{Handle.LOGIN_COURIER}', data=request_body)
 
     @allure.title('Авторизация под курьером выдает id')
     def test_courier_log_in(self):
-        response = requests.post(
-            f'{Urls.URL}{Handle.LOGIN_COURIER}',
-            data=Users.data_current)
-        assert response.status_code == 200 and 'id' in response.text
+        response = self._login_courier(request_body=Users.data_current)
+        assert response.status_code == 200
+        assert response.json().get("id") is not None
+        assert response.json().get("id") > 0
 
     @allure.title('Ошибка при авторизации если логин или пароль не корректные')
     def test_courier_log_negative(self):
-        response = requests.post(
-            f'{Urls.URL}{Handle.LOGIN_COURIER}',
-            data=Users.data_negative)
-        assert response.status_code == 404 and 'Учетная запись не найдена' in response.text
+        response = self._login_courier(request_body=Users.data_negative)
+        assert response.status_code == 404
+        assert response.json().get("message") == Courier.response_login_negative
 
     @pytest.mark.parametrize('data_without_login_or_password', [Users.data_without_login, Users.data_without_password])
     @allure.title('Ошибка при авторизации если не зполнить логин или пароль')
     def test_courier_log_not_all_data(self, data_without_login_or_password):
-        response = requests.post(
-            f'{Urls.URL}{Handle.LOGIN_COURIER}',
-            data=data_without_login_or_password)
-        assert response.status_code == 400 and 'Недостаточно данных для входа' in response.text
+        response = self._login_courier(request_body=data_without_login_or_password)
+        assert response.status_code == 400
+        assert response.json().get("message") == Courier.response_login_not_full_data
